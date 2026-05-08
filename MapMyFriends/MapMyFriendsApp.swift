fafdfca0c2fea9c83510acecc5a1a34aa545555f
mfyameen <mfyameen@gmail.com>
2026-05-08 15:36:59 -0400

    Initial Commit

---
+++ MapMyFriends/MapMyFriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapMyFriendsApp.swift
+//  MapMyFriends
+//
+//  Created by Fazal Yameen on 5/8/26.
+//
+
+import SwiftUI
+
+@main
+struct MapMyFriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
